c543de5444c0c070a9d6201620133ccb3c2aeb98
Sam Geen <samgeen@gmail.com>
2014-06-03 17:31:20 +0200

    Added units to interface, rejigged stuff

--- ramses/bin/makef2py.sh
+++ ramses/bin/makef2py.sh
@@ -1,3 +1,3 @@
-f2py -m rampy ../patch/rampy/ramses.f90 -h rampy.pyf --overwrite-signature
+f2py -m ramses ../patch/rampy/ramses.f90 -h ramses.pyf --overwrite-signature
 #f2py -m rampy -h rampy.pyf ../amr/ramses.f90 
-f2py -lgfortran -c *.o rampy.pyf
+f2py -lgfortran -c *.o ramses.pyf
--- ramses/patch/rampy/ramses.f90
+++ ramses/patch/rampy/ramses.f90
@@ -2,20 +2,27 @@
 
 module data
   use pm_commons
+  use amr_commons,only:units_density,units_time,units_length
   implicit none
-!f2py   real(dp),allocatable,dimension(:,:)::xp       ! Positionsr
-!f2py   real(dp),allocatable,dimension(:,:)::vp       ! Velocities
-!f2py   real(dp),allocatable,dimension(:)  ::mp       ! Masses
-!f2py   real(dp),allocatable,dimension(:)  ::tp       ! Birth epoch
+! PARTICLE DATA
+!f2py   real(kind=8),allocatable,dimension(:,:)::xp       ! Positions
+!f2py   real(kind=8),allocatable,dimension(:,:)::vp       ! Velocities
+!f2py   real(kind=8),allocatable,dimension(:)  ::mp       ! Masses
+!f2py   real(kind=8),allocatable,dimension(:)  ::tp       ! Birth epoch
+! UNITS DATA (RAW VALUES ONLY)
+!f2py   real(kind=8)::units_density
+!f2py   real(kind=8)::units_time
+!f2py   real(kind=8)::units_length
+
+contains
+  subroutine init
+    !f2py threadsafe
+    call set_jump_init
+  end subroutine init
+    
+  subroutine step
+    !f2py threadsafe
+    call set_jump_step
+  end subroutine step
 
 end module data
-
-subroutine init
-  !f2py threadsafe
-  call set_jump_init
-end subroutine init
-
-subroutine step
-  !f2py threadsafe
-  call set_jump_step
-end subroutine step
